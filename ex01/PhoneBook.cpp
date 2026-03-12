bcee58e6204eb7b18cc425a291005248d0df8e40
Weijiang Yang <weiyang@f4r12s16.clusters.42paris.fr>
2026-03-12 16:28:17 +0100

    commit

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -26,35 +26,45 @@ std::string format(std::string str)
 
 void PhoneBook::searchContact()
 {
-	int index;
-	std::string line;
-
-	std::cout << std::setw(10) << "index" << "|"
-			  << std::setw(10) << "first name" << "|"
-			  << std::setw(10) << "last name" << "|"
-			  << std::setw(10) << "nick name"
-			  << std::endl;
-	for (int i = 0; i < total; i++)
+    int index;
+    std::string line;
+	
+    std::cout << std::setw(10) << "index" << "|"
+              << std::setw(10) << "first name" << "|"
+              << std::setw(10) << "last name" << "|"
+              << std::setw(10) << "nick name"
+              << std::endl;
+    for (int i = 0; i < total; i++)
+    {
+        std::cout << std::setw(10) << i << "|"
+                  << std::setw(10) << format(contacts[i].getFirstName()) << "|"
+                  << std::setw(10) << format(contacts[i].getLastName()) << "|"
+                  << std::setw(10) << format(contacts[i].getNickName())
+                  << std::endl;
+    };
+   	while (true)
 	{
-		std::cout << std::setw(10) << i << "|"
-				  << std::setw(10) << format(contacts[i].getFirstName()) << "|"
-				  << std::setw(10) << format(contacts[i].getLastName()) << "|"
-				  << std::setw(10) << format(contacts[i].getNickName())
-				  << std::endl;
-	};
-	std::cout << "Enter index: ";
-	std::getline(std::cin, line);
-	std::stringstream ss(line);
-	if (!(ss >> index))
-	{
-		std::cout << "Invalid index" << std::endl;
-		return;
+		std::cout << "Enter index: ";
+		if (!std::getline(std::cin, line))
+			return ;
+		std::stringstream ss(line);
+		if (!(ss >> index))
+		{
+			std::cout << "Invald index"<< std::endl;
+			continue;
+		}
+		if ((index >= total || index < 0) && total)
+		{
+			std::cout << "There is not this index in the list of contacts" << std::endl;
+			continue;
+		}
+		if (total == 0)
+		{
+			std::cout << "There is not any contact in the list" << std:: endl;
+			break;
+		}
+		break;
 	}
-	if (index >= total || index < 0)
-	{
-		std::cout << "there is not this index in the list of contacts" << std::endl;
-		return ;
-	}
-	if (total > 0 && index >= 0 && index < total)
-		contacts[index].displayContact();
+    if (total > 0)
+        contacts[index].displayContact();
 }
